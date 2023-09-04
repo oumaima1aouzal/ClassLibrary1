c8b82aaf4c807d813f2dbd11462d1b7437d3f2bf
oumaima1aouzal <oumaima1aouzal@gmail.com>
2023-09-04 16:51:23 +0200

    Ajoutez des fichiers projet.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+
+    }
+}
